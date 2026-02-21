ae87ef5c7d1ef470034648366eceb6f11f1141b6
ersultananime <68365448+ersultananime@users.noreply.github.com>
2026-02-21 15:02:34 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
 print("Hello World!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World! Сәлем, бұл біздің жеткізу қызметінің API-ы"}
